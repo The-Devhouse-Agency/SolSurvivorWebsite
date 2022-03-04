f59d92c19772d672f57693947e90d905b36d0952
ericjames3681 <eric@thedevhouseagency.com>
2022-03-04 17:36:49 -0600

    oops Images...

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
     <div>
       <BackgroundVideo/>      
       <Header  />
-      <Gallery />
+      <Gallery data={landingPageData.Gallery}/>
       <Features />
       <Roadmap />
       <Contact  />
